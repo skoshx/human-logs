b479e92619c79297066830660cd961172f360b26
Rasmus Gustafsson <rasmus.gustafsson2611@gmail.com>
2024-07-26 05:21:37 +0300

    test: update vitest config

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,11 +1,8 @@
-/// <reference types="vitest" />
-
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
 	test: {
 		coverage: {
-			'100': true,
 			include: ['src'],
 			reporter: ['text', 'json', 'html']
 		}
